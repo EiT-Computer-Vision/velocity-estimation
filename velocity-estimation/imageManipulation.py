d0ee2233dbe9281dca3cd467e3e23cf914bfb937
KjetilSekseKristiansen <kjetsekse@gmail.com>
2020-02-19 09:50:06 +0100

    it aint much but its honest work

---
+++ velocity-estimation/imageManipulation.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import glob
+from matplotlib import pyplot as plt
+import numpy as np
+import pykitti as pk
+
+def loadData():
+    basedir = 'C:\\Users\\Kjetk\\PycharmProjects\\velocityEstimationStereo'
+    date = '2011_09_26'
+    drive = '0096'
+
+
+# The 'frames' argument is optional - default: None, which loads the whole dataset.
+# Calibration, timestamps, and IMU data are read automatically.
+# Camera and velodyne data are available via properties that create generators
+# when accessed, or through getter methods that provide random access.
+    data = pk.raw(basedir, date, drive, frames=range(0, 400, 5))
+    return data
+# dataset.calib:         Calibration data are accessible as a named tuple
+# dataset.timestamps:    Timestamps are parsed into a list of datetime objects
+# dataset.oxts:          List of OXTS packets and 6-dof poses as named tuples
+# dataset.camN:          Returns a generator that loads individual images from camera N
+# dataset.get_camN(idx): Returns the image from camera N at idx
+# dataset.gray:          Returns a generator that loads monochrome stereo pairs (cam0, cam1)
+# dataset.get_gray(idx): Returns the monochrome stereo pair at idx
+# dataset.rgb:           Returns a generator that loads RGB stereo pairs (cam2, cam3)
+# dataset.get_rgb(idx):  Returns the RGB stereo pair at idx
+# dataset.velo:          Returns a generator that loads velodyne scans as [x,y,z,reflectance]
+# dataset.get_velo(idx): Returns the velodyne scan at idx
+
+#loads the camera to camera calibration matrix as a dictionary
